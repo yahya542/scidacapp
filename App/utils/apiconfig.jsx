bc38d6cd81275dea1f6146fbe98e1f43fb694898
yahya <muhyahyaas@gmail.com>
2025-06-19 11:03:25 +0700

    hapus api keys dari public

--- App/utils/apiconfig.jsx
+++
@@ -1,7 +0,0 @@
-// src/config/aiConfig.js
-
-export const AI_API_CONFIG = {
-  endpoint: 'https://openrouter.ai/api/v1/chat/completions',
-  apiKey: 'Bearer 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',
-  model: 'deepseek/deepseek-r1-0528-qwen3-8b:free'
-};
